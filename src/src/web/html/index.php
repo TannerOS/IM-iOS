50704dd8c3e2c396d1ff1a26627a6da02c033ca7
Tanner OS <Tanner@codeshield.org>
2016-03-08 12:40:11 -0500

    Update index.php

--- src/src/web/html/index.php
+++ src/src/web/html/index.php
@@ -15,6 +15,15 @@
       <header>
         <h1>ImagicalMine</h1>
       </header>
+      
+      <nav>
+        <div class="navbar">
+          <a href="#">Home</a>
+          <a href="#">Servers</a>
+          <a href="ranks.html">Ranks</a>
+          <a href="#">Staff</a>
+        </div>
+      </nav>
 
     </body>
 
